1a6f7a2c234c7f6a35103e17b85fdce432267238
KindNet Development <dev@kindnet.com>
2025-10-07 21:58:50 +0530

    feat: add ESLint configuration for code quality enforcement

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error'
+  }
+};
